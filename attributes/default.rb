9d2d143e3208bf5d3f5c095d4a16afd427ca90c9
Thomas Hollstegge <thomas.hollstegge@zweitag.de>
2014-04-30 13:47:20 +0200

    Rename build essential attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default[:gpg][:keys_data_bag] = 'gpg_keys'
 default[:gpg][:user] = 'root'
 default[:gpg][:homedir] = '.gnupg'
 
-default[:build_essential][:compiletime] = true
+default['build-essential']['compile_time'] = true
